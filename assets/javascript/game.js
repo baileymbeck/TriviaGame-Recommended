7bc8bff55bdd3f46c2b1c15e4178502b97a2c984
Bailey Beck <baileymbeck@gmail.com>
2019-09-13 21:17:42 -0500

    added images, basic css to match imgs, init game code and variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+var start = document.getElementById("start");
+var quiz = document.getElementById("quiz");
+var qImg = document.getElementById("qImage");
+var question = document.getElementById("question");
+var counter = document.getElementById("counter");
+var timeGauge = document.getElementById("timeGauge");
+
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+
+var scoreCounter = document.getElementById("scoreCounter");
+
+let questions = [
+{   question: "how do you take your tea?",
+    imgSrc: "", 
+    choiceA:"",
+    choiceB: "",
+    choiceC: "",
+    correct: "A",
+}
+
+
+
+
+]

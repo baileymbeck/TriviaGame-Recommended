fed1601be31cee1886417293c1b757e943bb150f
Bailey Beck <baileymbeck@gmail.com>
2019-09-16 21:22:18 -0500

    made some changes, practicing js setup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,8 @@
 var start = document.getElementById("start");
 var quiz = document.getElementById("quiz");
-var qImg = document.getElementById("qImage");
 var question = document.getElementById("question");
-var counter = document.getElementById("counter");
-var timeGauge = document.getElementById("timeGauge");
 
-var choiceA = document.getElementById("A");
-var choiceB = document.getElementById("B");
-var choiceC = document.getElementById("C");
-
-var scoreCounter = document.getElementById("scoreCounter");
-
+// quiz questions
 let questions = [
 {   question: "What color is the sky?",
     imgSrc: "", 
@@ -37,3 +29,66 @@ let questions = [
     correct: "C"
 },
 ]
+// attempt to modify previous timer
+$("#start").on("click", function() {
+    
+    $("#quiz").css("display" , "inherit");
+    var sec = 10;
+    var time = setInterval(myTimer, 1000);
+
+    function myTimer() {
+        document.getElementById('time').innerHTML = sec + " left";
+        sec--;
+        if (sec == -1) {
+            clearInterval(time);
+    
+        }
+    }
+});
+
+// populate quiz container
+function buildQuiz() {
+    // we'll need a place to store the HTML output
+    const output = [];
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // we'll want to store the list of answer choices
+      const answers = [];
+
+      // and for each available answer...
+      for (letter in currentQuestion.answers) {
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("")} </div>`
+      );
+    });
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join("");
+  }
+
+
+// var start = document.getElementById("start");
+// var quiz = document.getElementById("quiz");
+// var qImg = document.getElementById("qImage");
+// var question = document.getElementById("question");
+// var counter = document.getElementById("counter");
+// var timeGauge = document.getElementById("timeGauge");
+
+// var choiceA = document.getElementById("A");
+// var choiceB = document.getElementById("B");
+// var choiceC = document.getElementById("C");
+
+// var scoreCounter = document.getElementById("scoreCounter");
